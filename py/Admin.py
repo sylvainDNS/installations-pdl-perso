dd30050718f079adb3eea798090609394998a00d
sylvainDNS <sylvain.denyse@gmail.com>
2016-02-04 10:19:11 +0100

    second

--- py/Admin.py
+++ py/Admin.py
@@ -1,9 +1,9 @@
 # Le composant Admin a pour rôle la création des tables de la base de données.
 
 import sqlite3
-conn = sqlite3.connect("/db/myDB.db")
 
 def connect():
+    conn = sqlite3.connect("/db/myDB.db")
     return conn.cursor()
 
 def disconnect():
@@ -11,23 +11,28 @@ def disconnect():
 
 def creerTable():
     c = connect()
-    c.execute('''CREATE TABLE installation (numero INTEGER PRIMARY KEY, nom TEXT, adresse TEXT, code_postal TEXT, ville TEXT, latitude INTEGER, longitude INTEGER)''')
-    c.execute('''CREATE TABLE equipement (numero INTEGER PRIMARY KEY, nom TEXT)''')
-    # manque id 
-    c.execute('''CREATE TABLE activite (numero INTEGER PRIMARY KEY, nom TEXT)''')
-    conn.close
+    c.execute('''CREATE TABLE installations (id INTEGER PRIMARY KEY, nom VARCHAR, adresse VARCHAR, code_postal VARCHAR, ville VARCHAR, latitude DECIMAL, longitude DECIMAL)''')
+    c.execute('''CREATE TABLE equipements (id INTEGER PRIMARY KEY, nom VARCHAR, id_installation INTEGER FOREIGN KEY, type VARCHAR)''')
+    c.execute('''CREATE TABLE activites (id INTEGER PRIMARY KEY, nom VARCHAR, id_equipement VARCHAR FOREIGN KEY)''')
+    c.execute('''CREATE TABLE equipements_activites (id_equipement INTEGER FOREIGN KEY, id_activite INTEGER FOREIGN KEY)''')
+    diconnect()
 
-def addInstallation(numero, nom, adresse, code_postal, ville, latitude, longitude):
+def addInstallations(id, nom, adresse, code_postal, ville, latitude, longitude):
     c = connect()
-    c.execute('''INSERT INTO installation (numero, nom, adresse, code_postal, ville, latitude, longitude) VALUES (?, ?, ?, ?, ?, ?, ?, ?)''', (numero, nom, adresse, code_postal, ville, latitude, longitude))
-    conn.close
+    c.execute('''INSERT INTO installations (id, nom, adresse, code_postal, ville, latitude, longitude) VALUES (?, ?, ?, ?, ?, ?, ?, ?)''', (id, nom, adresse, code_postal, ville, latitude, longitude))
+    diconnect()
 
-def addEquipement(numero, nom):
+def addEquipements(id, nom, id_installation, type):
     c = connect()
-    c.execute('''INSERT INTO equipement (numero, nom) VALUES (?, ?)''', (numero, nom))
-    conn.close
+    c.execute('''INSERT INTO equipements (id, nom, id_installation, type) VALUES (?, ?, ?, ?)''', (id, nom, id_installation, type))
+    diconnect()
 
-def addActivite(numero, nom):
+def addActivites(id, nom, id_equipement):
     c = connect()
-    c.execute('''INSERT INTO activite (numero, nom) VALUES (?, ?)''', (numero, nom))
-    conn.close
+    c.execute('''INSERT INTO activites (id, nom, id_equipement) VALUES (?, ?, ?)''', (id, nom, id_equipement))
+    diconnect()
+
+def addEquipementsActivites(id_equipement, id_activite):
+    c = connect()
+    c.execute('''INSERT INTO equipements_activites (id_equipement, id_activite) VALUES (?, ?)''', (id_equipement, id_activite))
+    diconnect()
--- py/Import.py
+++ py/Import.py
@@ -1,1 +1,32 @@
 # Le composant Import a pour rôle le remplissage des tables de la base de données à partir des fichiers CSV.
+
+import csv
+from Admin import *
+
+def BD():
+    creerTable()
+    
+def importInstallations():
+    fname = "../csv/installations_table.csv"
+    fichier = open(fname, 'r')
+
+    try:
+        lecteur = csv.reader(fichier)
+        for ligne in lecteur:
+            id = ligne[1]
+            nom = ligne[0]
+            adresse = ""
+            if ligne[5] != "":
+                adresse += ligne[5] + " "
+            if ligne[6] != "":
+                adresse += ligne[6] + " "
+            adresse += ligne[7]
+            code_postal = ligne[4]
+            ville = ligne[2]
+            latitude = ligne[9]
+            longitude = ligne[10]
+            addInstallation(id, nom, adresse, code_postal, ville, latitude, longitude)
+    finally:
+        fichier.close()
+
+BD()
