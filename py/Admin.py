505dd04f704a16dcb4993bf8f84ad8c364ee3c46
sylvainDNS <sylvain.denyse@gmail.com>
2016-02-04 10:50:37 +0100

    Fin de séance 2

--- py/Admin.py
+++ py/Admin.py
@@ -3,36 +3,45 @@
 import sqlite3
 
 def connect():
-    conn = sqlite3.connect("/db/myDB.db")
+    conn = sqlite3.connect("../db/myDB.db")
     return conn.cursor()
 
-def disconnect():
-    conn.close
+def disconnect(c):
+    c.close
 
 def creerTable():
     c = connect()
-    c.execute('''CREATE TABLE installations (id INTEGER PRIMARY KEY, nom VARCHAR, adresse VARCHAR, code_postal VARCHAR, ville VARCHAR, latitude DECIMAL, longitude DECIMAL)''')
-    c.execute('''CREATE TABLE equipements (id INTEGER PRIMARY KEY, nom VARCHAR, id_installation INTEGER FOREIGN KEY, type VARCHAR)''')
-    c.execute('''CREATE TABLE activites (id INTEGER PRIMARY KEY, nom VARCHAR, id_equipement VARCHAR FOREIGN KEY)''')
-    c.execute('''CREATE TABLE equipements_activites (id_equipement INTEGER FOREIGN KEY, id_activite INTEGER FOREIGN KEY)''')
-    diconnect()
+
+    # TABLE INSTALLATIONS
+    c.execute('''CREATE TABLE IF NOT EXISTS installations (id INTEGER PRIMARY KEY, nom VARCHAR, adresse VARCHAR, code_postal VARCHAR, ville VARCHAR, latitude DECIMAL, longitude DECIMAL)''')
+
+    # TABLE EQUIPEMENTS
+    c.execute('''CREATE TABLE IF NOT EXISTS equipements (id INTEGER PRIMARY KEY, nom VARCHAR, id_installation INTEGER, type VARCHAR, FOREIGN KEY(id_installation) REFERENCES installation(id))''')
+
+    # TABLE ACTIVITES
+    c.execute('''CREATE TABLE IF NOT EXISTS activites (id INTEGER PRIMARY KEY, nom VARCHAR, id_equipement VARCHAR, FOREIGN KEY(id_equipement) REFERENCES equipements(id))''')
+
+    # TABLE EQUIPEMENTS_ACTIVITES
+    c.execute('''CREATE TABLE IF NOT EXISTS equipements_activites (id_equipement INTEGER, id_activite INTEGER, FOREIGN KEY(id_equipement) REFERENCES equipements(id), FOREIGN KEY(id_activite) REFERENCES activites(id), PRIMARY KEY(id_equipement, id_activite))''')
+
+    disconnect(c)
 
 def addInstallations(id, nom, adresse, code_postal, ville, latitude, longitude):
     c = connect()
-    c.execute('''INSERT INTO installations (id, nom, adresse, code_postal, ville, latitude, longitude) VALUES (?, ?, ?, ?, ?, ?, ?, ?)''', (id, nom, adresse, code_postal, ville, latitude, longitude))
-    diconnect()
+    c.execute('''INSERT INTO installations (id, nom, adresse, code_postal, ville, latitude, longitude) VALUES (?, ?, ?, ?, ?, ?, ?)''', (id, nom, adresse, code_postal, ville, latitude, longitude))
+    disconnect(c)
 
 def addEquipements(id, nom, id_installation, type):
     c = connect()
     c.execute('''INSERT INTO equipements (id, nom, id_installation, type) VALUES (?, ?, ?, ?)''', (id, nom, id_installation, type))
-    diconnect()
+    disconnect(c)
 
 def addActivites(id, nom, id_equipement):
     c = connect()
     c.execute('''INSERT INTO activites (id, nom, id_equipement) VALUES (?, ?, ?)''', (id, nom, id_equipement))
-    diconnect()
+    disconnect(c)
 
 def addEquipementsActivites(id_equipement, id_activite):
     c = connect()
     c.execute('''INSERT INTO equipements_activites (id_equipement, id_activite) VALUES (?, ?)''', (id_equipement, id_activite))
-    diconnect()
+    disconnect(c)
--- py/Import.py
+++ py/Import.py
@@ -5,27 +5,35 @@ from Admin import *
 
 def BD():
     creerTable()
+    importInstallations()
     
 def importInstallations():
     fname = "../csv/installations_table.csv"
     fichier = open(fname, 'r')
-
+    
     try:
         lecteur = csv.reader(fichier)
+
+        passe = False
+        
         for ligne in lecteur:
-            id = ligne[1]
-            nom = ligne[0]
-            adresse = ""
-            if ligne[5] != "":
-                adresse += ligne[5] + " "
-            if ligne[6] != "":
-                adresse += ligne[6] + " "
-            adresse += ligne[7]
-            code_postal = ligne[4]
-            ville = ligne[2]
-            latitude = ligne[9]
-            longitude = ligne[10]
-            addInstallation(id, nom, adresse, code_postal, ville, latitude, longitude)
+            if passe == False:
+                passe = True
+            else:
+                id = int(ligne[1])
+                nom = ligne[0]
+                adresse = ""
+                if ligne[5] != "":
+                    adresse += ligne[5] + " "
+                if ligne[6] != "":
+                    adresse += ligne[6] + " "
+                adresse += ligne[7]
+                code_postal = ligne[4]
+                ville = ligne[2]
+                latitude = float(ligne[9])
+                longitude = float(ligne[10])
+                addInstallations(id, nom, adresse, code_postal, ville, latitude, longitude)
+                print("added")
     finally:
         fichier.close()
 
