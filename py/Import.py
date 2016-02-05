387320065e4178b92dd2c4244cd58b3581dadc2a
sylvainDNS <sylvain.denyse@gmail.com>
2016-02-05 14:31:08 +0100

    Suite de l'implementation de l'import des csv dans la BD

--- py/Import.py
+++ py/Import.py
@@ -6,35 +6,72 @@ from Admin import *
 def BD():
     creerTable()
     importInstallations()
-    
+    importEquipements()
+    importActivites()
+
 def importInstallations():
     fname = "../csv/installations_table.csv"
     fichier = open(fname, 'r')
-    
+
     try:
         lecteur = csv.reader(fichier)
 
         passe = False
-        
+
         for ligne in lecteur:
             if passe == False:
                 passe = True
             else:
-                id = int(ligne[1])
-                nom = ligne[0]
                 adresse = ""
                 if ligne[5] != "":
                     adresse += ligne[5] + " "
                 if ligne[6] != "":
                     adresse += ligne[6] + " "
                 adresse += ligne[7]
-                code_postal = ligne[4]
-                ville = ligne[2]
-                latitude = float(ligne[9])
-                longitude = float(ligne[10])
-                addInstallations(id, nom, adresse, code_postal, ville, latitude, longitude)
-                print("added")
+
+                addInstallations(int(ligne[1]), ligne[0], adresse, ligne[4], ligne[2], float(ligne[9]), float(ligne[10]))
+
+                # print("installation_added")
     finally:
         fichier.close()
+
+def importEquipements():
+    fname = "../csv/equipements_table.csv"
+    fichier = open(fname, 'r')
+
+    try:
+        lecteur = csv.reader(fichier)
+
+        passe = False
+
+        for ligne in lecteur:
+            if passe == False:
+                passe = True
+            else:
+                addEquipements(int(ligne[4]), ligne[5], int(ligne[3]), ligne[7])
+                # print("equipement_added")
+    finally:
+        fichier.close()
+
+def importActivites():
+    fname = "../csv/activites_table.csv"
+    fichier = open(fname, 'r')
+
+    try:
+        lecteur = csv.reader(fichier)
+
+        passe = False
+
+        for ligne in lecteur:
+            if passe == False:
+                passe = True
+            else:
+                addActivites(int(ligne[4]), ligne[5], int(ligne[2]))
+                # print("activite_added")
+    finally:
+        fichier.close()
+
+def defEquipementsActivites():
+    return -1
 
 BD()
