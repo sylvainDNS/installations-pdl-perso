92aa78fcc833b37aba2ea4928935411f1c1e8988
sylvainDNS <sylvain.denyse@gmail.com>
2016-02-25 10:53:15 +0100

    Ajout de service rest

--- py/Service.py
+++ py/Service.py
@@ -1,4 +1,9 @@
 # Le composant Service a pour rôle d'exposer les fonctionnalités de l'application, au travers de services REST.
 
+from libs.bottle import route, template, run
 
-import bottle
+@route('/hello/<name>')
+def index(name):
+    return template('<b>hello {{name}}</b>!', name = name)
+
+run(host = 'localhost', port = 8080)
