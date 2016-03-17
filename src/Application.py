8929eb39d5f19983f63b887f2aa29bd099ddb1cb
sylvainDNS <sylvain.denyse@gmail.com>
2016-03-17 12:18:58 +0100

    Ajout des requêtes / Bootstrap

--- src/Application.py
+++ src/Application.py
@@ -1,1 +1,44 @@
 from libs.bottle import route, template, run, static_file, error, view
+from admin.DbAdmin import db, cursor, disconnect
+import json
+
+conn = db()
+my_cursor = cursor(conn)
+
+# Définition de la page d'accueil
+@route('/')
+def index():
+    return server_static('index.html')
+
+@route('/activite/<query>')
+def activite(query):
+    # On reforme la string
+    query.replace("%20", " ")
+    my_query = query_db('SELECT DISTINCT i.ville FROM installations i JOIN equipements e ON i.id = e.id_installation JOIN equipements_activites ea ON e.id = ea.id_equipement JOIN activites a ON ea.id_activite = a.id WHERE a.nom = ' + '"' + query + '"')
+    return my_query
+
+@route('/commune/<query>')
+def commune(query):
+    my_query = query_db('SELECT nom FROM installations WHERE ville = ' + '"' + query + '"')
+    return my_query
+
+# Défintion de la racine du serveur
+@route('/<filename>')
+def server_static(filename):
+    return static_file(filename, root='public')
+
+@route('/<filepath:path>')
+def server_static(filepath):
+    return static_file(filepath, root='public')
+
+@error(404)
+def error404(error):
+    return "La page n'existe pas"
+
+def query_db(query, one=False):
+    my_cursor.execute(query)
+    result = my_cursor.fetchall()
+    disconnect(my_cursor)
+    return json.dumps(result)
+
+run(host = 'localhost', port = 8080)
---
+++ src/public/js/search_conflict-20160317-105215.js
@@ -0,0 +1,53 @@
+$(function() {
+
+    $('#btnRecherche').on('click', function(e) {
+        e.preventDefault();
+
+        var r = document.getElementById('recherche').value;
+        if(document.getElementById('choixRecherche1').checked == false){
+            var c = 'activite';
+        }else{
+            var c = 'commune';
+        }
+
+        // var json1 = $.ajax({
+        //     url: c + '/' + r,
+        //     method: 'get'
+        // });
+        //
+        // var json2 = $.ajax({
+        //     url: c.str.split(' ').join('-') + '/' + r,
+        //     method: 'get'
+        // });
+        //
+        // var ajax = json1.concat(json2);
+
+        var ajax = $.ajax({
+            url: c + '/' + r,
+            method: 'get'
+        });
+
+        ajax.done(function(data) {
+            console.log('réponse reçue !');
+            console.log(data);
+        });
+
+        ajax.fail(function() {
+            console.log('erreur');
+        })
+
+        // recherche();
+    })
+
+});
+
+
+// function recherche(){
+//     var e = document.getElementById('recherche').value;
+//     if(document.getElementById('choixRecherche1').checked = true){
+//         var c = 'activite';
+//     }else{
+//         var c = 'commune';
+//     }
+//     document.location.href = c + '/' + e;
+// }
